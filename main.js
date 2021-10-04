37c4fcb5c945c60efd55279199b3ffb360ff9b6c
Satsuki-Isobe <isobe@plusjam.jp>
2021-10-04 23:58:03 +0900

    test

--- main.js
+++ main.js
@@ -3,19 +3,7 @@ const http = require(`http`)
 const httpStatus = require(`http-status-codes`)
 const port = 3000
 
-const app = http.createServer((req, res) => {
-    console.log(`Received an incoming request!`)
-    
-    res.writeHead(httpStatus.OK, {
-        "Content-Type": "text/html"
-    })
-
-    let responseMessage = "<h1>Hello, Universe</h1>"
-    res.write(responseMessage)
-    res.end()
-
-    console.log('Sent a response : ', responseMessage)
-})
+const app = http.createServer()
 
 app.listen(port)
 
