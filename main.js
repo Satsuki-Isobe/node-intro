55621de574ef00607a97ce3703bdd46a1b4ea084
Satsuki-Isobe <isobe@plusjam.jp>
2021-10-05 00:30:09 +0900

    step

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-const { response } = require("express")
 const http = require(`http`)
 const httpStatus = require(`http-status-codes`)
 const port = 3000
 
 const app = http.createServer()
+
+const getJSONString = obj => {
+    return JSON.stringify(obj, null, 2)
+}
+
+app.on("request", (req, res) => {
+    let body = []
+    req.on("data", (bodyData) => {
+        body.push(bodyData)
+    })
+
+    req.on("end", () => {
+        body = Buffer.concat(body).toString()
+        console.log("リクエスト", body)
+    })
+    
+    console.log(getJSONString(req.method))
+    console.log(getJSONString(req.url))
+    console.log(getJSONString(req.headers))
+    
+    res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+    })
+    let responseMessage = "<h1>Hello</h1>"
+    res.end(responseMessage)
+})
 
 app.listen(port)
 
