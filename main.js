468bc71346a893a6623a3c0956a366626ee4e4ee
Satsuki-Isobe <isobe@plusjam.jp>
2021-10-04 23:52:13 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { response } = require("express")
+const http = require(`http`)
+const httpStatus = require(`http-status-codes`)
+const port = 3000
+
+const app = http.createServer((req, res) => {
+    console.log(`Received an incoming request!`)
+    
+    res.writeHead(httpStatus.OK, {
+        "Content-Type": "text/html"
+    })
+
+    let responseMessage = "<h1>Hello, Universe</h1>"
+    res.write(responseMessage)
+    res.end()
+
+    console.log('Sent a response : ', responseMessage)
+})
+
+app.listen(port)
+
+console.log(`The server has started and is listning on port number ${port}`)
